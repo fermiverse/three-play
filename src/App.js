adbbfde644dc790d3f2f1f1b891a90dcc223091a
fermi <fermiverse@gmail.com>
2021-06-10 01:15:17 +0530

    Add 3d playground and initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import React, { Suspense, useRef } from 'react';
+
+function Model() {
+  const gltf = useGLTF("/ship.glb", true);
+  return <primitive object={gltf.scene} dispose={null} />
+}
+
+function HTMLContent() {
+  const ref = useRef();
+  return (
+    <mesh ref={ref}>
+      <Model />
+    </mesh>
+  )
+}
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="plot-container">
+        <Canvas
+        concurrent
+        colorManagement
+        camera={{ position: [0, 0, 120], fov: 70 }}>
+          <OrbitControls />
+          <Suspense fallback={null}>
+          <HTMLContent />
+          </Suspense>
+          <ambientLight intensity={0.4} />
+          <directionalLight intensity={0.4} position={[30, 100, 9]} />
+          <directionalLight intensity={0.7} position={[0, -100, 90]} />
+          <pointLight position={[100, 100, 100]} intensity={0.1} />
+        </Canvas>
+      </div>
     </div>
   );
 }
